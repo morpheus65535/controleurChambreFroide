bccdfd4d85dcad32386184b8bb8f9ae44fa3ae60
morpheus65535 <louis_vezina@hotmail.com>
2023-01-14 21:12:37 -0500

    WIP

--- relay.py
+++ relay.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from time import time, sleep
+from time import sleep
 
 from config import settings
 from sensor import interior_temperature
@@ -32,38 +32,40 @@ relay = Relay()
 def set_required_relay_state():
     from weather import get_latest_logged_exterior_temp
 
-    t_end = time() + 60 * 10
-    while time() < t_end:
+    while True:
         sleep(60)
-        if settings.getboolean('general', 'enabled'):
-            temp_int = interior_temperature.temperature
-            temp_ext = get_latest_logged_exterior_temp()
-            temp_low = settings.getint('general', 'temp_low')
-            temp_high = settings.getint('general', 'temp_high')
-            if not all([temp_int, temp_ext, temp_low, temp_high]):
-                continue
+        try:
+            if settings.getboolean('general', 'enabled'):
+                temp_int = interior_temperature.temperature
+                temp_ext = get_latest_logged_exterior_temp()
+                temp_low = settings.getint('general', 'temp_low')
+                temp_high = settings.getint('general', 'temp_high')
+                if not all([temp_int, temp_ext, temp_low, temp_high]):
+                    continue
 
-            if temp_int < temp_low:
-                # it's too cold in the cold room
-                if temp_ext > temp_low:
-                    # we use heat from outdoor
-                    relay.set_high()
-                    update_last_relay_state()
-                else:
-                    # we shut it down because outdoor can't help us
+                if temp_int < temp_low:
+                    # it's too cold in the cold room
+                    if temp_ext > temp_low:
+                        # we use heat from outdoor
+                        relay.set_high()
+                        update_last_relay_state()
+                    else:
+                        # we shut it down because outdoor can't help us
+                        relay.set_low()
+                elif temp_low <= temp_int <= temp_high:
+                    # we're in the desired range, we can take a brake!
                     relay.set_low()
-            elif temp_low <= temp_int <= temp_high:
-                # we're in the desired range, we can take a brake!
-                relay.set_low()
-            elif temp_int > temp_high:
-                # it's too hot in the cold room
-                if temp_ext < temp_high:
-                    # we use cold from outdoor
-                    relay.set_high()
-                    update_last_relay_state()
-                else:
-                    # we shut it down because outdoor can't help us
-                    relay.set_low()
+                elif temp_int > temp_high:
+                    # it's too hot in the cold room
+                    if temp_ext < temp_high:
+                        # we use cold from outdoor
+                        relay.set_high()
+                        update_last_relay_state()
+                    else:
+                        # we shut it down because outdoor can't help us
+                        relay.set_low()
+        except Exception:
+            break
 
 
 def update_last_relay_state():
--- scheduler.py
+++ scheduler.py
@@ -2,11 +2,13 @@
 import os
 
 import tzlocal
+import contextlib
 
 from datetime import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.schedulers import SchedulerAlreadyRunningError
 
 from weather import log_current_temp, forecast
 if 'NO_SENSOR' not in os.environ:
@@ -20,18 +22,22 @@ class Scheduler:
     def __init__(self):
         self.aps_scheduler = BackgroundScheduler(timezone=str(tzlocal.get_localzone()))
 
-        self.aps_scheduler.add_job(log_current_temp, IntervalTrigger(minutes=15), max_instances=1, id='log_temp')
+    def configure(self):
+        self.aps_scheduler.add_job(log_current_temp, IntervalTrigger(minutes=15), max_instances=1, id='log_temp',
+                                   replace_existing=True)
         self.aps_scheduler.add_job(forecast.update_forecast, IntervalTrigger(minutes=15), max_instances=1,
-                                   id='forecast')
+                                   id='forecast', replace_existing=True)
         if 'NO_SENSOR' not in os.environ:
             self.aps_scheduler.add_job(interior_temperature.get_temperature_int, IntervalTrigger(minutes=1),
-                                       max_instances=1, id='temp_int')
+                                       max_instances=1, id='temp_int', replace_existing=True)
         if 'NO_RELAY' not in os.environ:
-            self.aps_scheduler.add_job(set_required_relay_state, IntervalTrigger(minutes=15),
-                                       max_instances=1, id='set_relay')
+            self.aps_scheduler.add_job(set_required_relay_state, IntervalTrigger(minutes=1),
+                                       max_instances=1, id='set_relay', replace_existing=True)
 
     def start(self):
-        self.aps_scheduler.start()
+        self.configure()
+        with contextlib.suppress(SchedulerAlreadyRunningError):
+            self.aps_scheduler.start()
         self.run_once()
 
     @staticmethod
